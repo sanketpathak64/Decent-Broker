3f2c770f98264859c94e94f1ef2b32de7e21c8f6
sanket pathak <sanketpathak64@gmail.com>
2020-05-16 17:18:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app =next({
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 app.prepare().then(()=>{
-  createServer(handler).listen(3000,(err)=>{
+  createServer(handler).listen(process.env.PORT,(err)=>{
     if(err) throw err;
-    console.log('Ready on localhost:3000');
+    console.log('Ready on localhost:'+process.env.PORT);
   });
 });
